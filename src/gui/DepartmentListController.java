0277d913836ca2a7a096a4ca5fd8bd4df2746d83
Beto Seideman <betoseideman@gmail.com>
2022-01-13 17:36:00 -0300

    Department combobox

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -107,6 +107,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loanding view", e.getMessage(), AlertType.ERROR);
 		}
 	}
